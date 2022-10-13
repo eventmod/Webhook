ea9fd55fcdb7508b74aecdf0e218dd0a190342f7
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-10-13 19:24:09 +0700

    Split File

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import fetch from 'node-fetch';
 import express from 'express';
 import bodyParser from 'body-parser';
-import request from 'request';
-// var express = require('express')
-// var bodyParser = require('body-parser')
-// var request = require('request')
-// var fetch = require('node-fetch')
+import messageFunction from './message-function.js';
 var app = express()
 
 app.use(bodyParser.json())
@@ -19,49 +15,24 @@ app.post('/api', async (req, res) => {
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
   
-  const response = await fetch(`https://api.line.me/v2/bot/profile/${sender}`, {method: "GET", headers: {
+  const response = await fetch(`https://api.line.me/v2/bot/profile/${sender}`, {
+    method: "GET", 
+    headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer VXLwVklleTDFgPnUoLZbiYaiCOZJSxVKN5fWM1ggbqOY1knDJ8PV+N7e5mUK5Cq/hGW2mk2mcVGl+rZ33++9XMqzt6e+BTX7EhV+T/Pj6zzBV08QOi6yO4ErNcw0OU2ijJjEXEQ5M3g0ctwyb1hPmAdB04t89/1O/w1cDnyilFU='
-  }})
+    }
+  })
   var user = await response.json()
-
-  console.log(user)
 
   console.log(text, sender, replyToken)
   console.log(typeof sender, typeof text)
   // console.log(req.body.events[0])
   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    await sendText(sender, text, user.displayName)
+    await messageFunction.sendText(sender, user.displayName)
     console.log(user.displayName)
   }
   res.sendStatus(200)
 })
-
-async function sendText (sender, text, displayName) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'ยินดีต้อนรับคุณ ' + displayName + ' เข้าสู่ EventMod'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer VXLwVklleTDFgPnUoLZbiYaiCOZJSxVKN5fWM1ggbqOY1knDJ8PV+N7e5mUK5Cq/hGW2mk2mcVGl+rZ33++9XMqzt6e+BTX7EhV+T/Pj6zzBV08QOi6yO4ErNcw0OU2ijJjEXEQ5M3g0ctwyb1hPmAdB04t89/1O/w1cDnyilFU='
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
