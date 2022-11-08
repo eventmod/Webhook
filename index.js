8bfac9ca654d40508adb84c8a3c4ce70f53c1446
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-11-08 10:26:19 +0700

    [ADD] Restart on failure in compose

--- index.js
+++ index.js
@@ -31,31 +31,6 @@ app.post('/api', async (req, res) => {
   if (requestText === 'List') {
     await responseFunction.sendEvent(sender, event)
   }
-
-  // if (requestText === 'Verify KMUTT') {
-  //   const text = "Please Send studentID 11 digits & KMUTT Mail \n\n Like this form ... \n\n 62130500000,example@kmutt.ac.th"
-  //   await responseFunction.sendText(sender, text)
-  // }
-
-  // if (requestText.split(",")[0].length === 11 && (requestText.split(",")[1].split("@")[1] === "kmutt.ac.th" || requestText.split(",")[1].split("@")[1] === "mail.kmutt.ac.th")) {
-  //   const studentID = requestText.split(",")[0]
-  //   const kmuttMail = requestText.split(",")[1]
-  //   console.log("studentID: " + studentID)
-  //   console.log("KMUTT Mail: " + kmuttMail)
-  // }
-
-  // const patternEmail = requestText.split(" ")
-  // if (patternEmail[0] === 'Email:') {
-  //   const email = patternEmail[1]
-  //   const componentEmail = email.split("@")
-  //   if(componentEmail[1] === 'mail.kmutt.ac.th' || componentEmail[1] === 'kmutt.ac.th'){
-  //     connection.query(`INSERT INTO lineaccounts (lineacc_userid, lineacc_studentid, lineacc_kmuttmail) VALUES ("${user.userId}", 'xxxxxxxxxxx', "${email}")`, (err, result) => {
-  //       if(err) throw err
-  //       console.log("Success")
-  //       console.log(result)
-  //     })
-  //   }
-  // }
   res.sendStatus(200)
 })
 
