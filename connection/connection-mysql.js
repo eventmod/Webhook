a0ee16f5ac70a74951334ef235bee4eb8bf1de09
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-11-09 08:51:27 +0700

    Test 1

--- connection/connection-mysql.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2';
-
-export const connection = mysql.createConnection({
-  host: 'eventmod.sit.kmutt.ac.th',
-  port: 55018,
-  user: 'Eventmod',
-  password: 'eventadmin',
-  database: 'eventmod'
-})
