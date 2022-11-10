64dce5c9206d3e9f937346d6f7d15458e635861d
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-11-10 22:51:38 +0700

    🔫 Test Join Event 3

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ app.post('/api', async (req, res) => {
   } else if(requestEvent.type === 'postback') {
     const dataPostback = requestEvent.postback.data
     const split = dataPostback.split("@")
-    const action = split[0].split("=")[1]
-    const eventId = split[1].split("=")[1]
+    // const action = split[0].split("=")[1]
+    // const eventId = split[1].split("=")[1]
 
     console.log(split)
     console.log(split[0])
     console.log(split[1])
-    console.log(action)
-    console.log(eventId)
+    // console.log(action)
+    // console.log(eventId)
 
     // const eventTitle = ""
     // connection.query('SELECT event_title FROM events WHERE event_id = ' + eventId, async (err, result) => {
